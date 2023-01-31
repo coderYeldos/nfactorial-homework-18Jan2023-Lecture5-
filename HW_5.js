517a119df2cd86b02b255d642a04852d838d6af3
Yeldos Dauletkhanuly <yeldos.dauletkhanuly@nu.edu.kz>
2023-01-31 11:23:45 +0600

    HW5

---
+++ HW_5.js
@@ -0,0 +1,25 @@
+// Part 1
+const elemOneById = document.getElementById('age-table label');
+
+// Part 2
+elemOneById.getElementsByTagName('label');
+
+// Part 3
+elemOneById.getElementsByTagName('td')[0];
+
+// Part 4
+const part4 = document.querySelectorAll('form[name="search"]');
+
+
+// Part 5
+const part5 = part4.getElementsByTagName('input')[0];
+
+
+// Part 6
+const part6 = part4.getElementsByTagName('input');
+part6[part6.length - 1];
+
+
+// This code of line is commenting content of body
+// and assigning it to the content of child node
+
